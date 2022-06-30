62dc4538c4e283b48bf32365283c1afb076ac56c
Frank Gabel <frank240690@web.de>
2022-06-30 15:49:02 +0200

    Number of classes change.

--- mmdet/apis/inference.py
+++ mmdet/apis/inference.py
@@ -168,6 +168,7 @@ def slided_inference_detector(model, img_path, slide_size, chip_size):
     """
     classnames = ['Toilet', 'Sink', 'Shower', 'Bathtub', 'Parking Lot', 'Dryer',
                   'Single Swing Door', 'Double Swing Door', 'Sliding Door']
+    classnames = ['Single Swing Door']
     img = mmcv.imread(img_path)
     height, width, channel = img.shape
     slide_h, slide_w = slide_size
