e8761f60d1ea42f1b5daa619aa29526824e42032
Frank <frank.k.gabel@gmail.com>
2022-08-12 10:25:49 +0200

    Remove debug prints.

--- mmdet/apis/inference.py
+++ mmdet/apis/inference.py
@@ -172,8 +172,7 @@ def slided_inference_detector(model, img, slide_size, chip_size):
     """
     classnames = ['Toilet', 'Sink', 'Shower', 'Bathtub', 'Parking Lot', 'Dryer',
                   'Single Swing Door', 'Double Swing Door', 'Sliding Door']
-    print(len(img))
-    print(type(img[0]))
+
     img = img[0]
     height, width, channel = img.shape
     slide_h, slide_w = slide_size
