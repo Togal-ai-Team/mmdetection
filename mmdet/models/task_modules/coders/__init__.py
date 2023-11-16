71e409ee9a71c7ca2e52071146fed5df1da1f765
frank <frank@togal.ai>
2023-11-16 16:56:24 +0100

    Add YOLOXBBoxCoder

--- mmdet/models/task_modules/coders/__init__.py
+++ mmdet/models/task_modules/coders/__init__.py
@@ -7,10 +7,10 @@ from .distance_point_bbox_coder import DistancePointBBoxCoder
 from .legacy_delta_xywh_bbox_coder import LegacyDeltaXYWHBBoxCoder
 from .pseudo_bbox_coder import PseudoBBoxCoder
 from .tblr_bbox_coder import TBLRBBoxCoder
-from .yolo_bbox_coder import YOLOBBoxCoder
+from .yolo_bbox_coder import YOLOBBoxCoder, YOLOXBBoxCoder
 
 __all__ = [
     'BaseBBoxCoder', 'PseudoBBoxCoder', 'DeltaXYWHBBoxCoder',
     'LegacyDeltaXYWHBBoxCoder', 'TBLRBBoxCoder', 'YOLOBBoxCoder',
-    'BucketingBBoxCoder', 'DistancePointBBoxCoder', 'DeltaXYWHBBoxCoderForGLIP'
+    'BucketingBBoxCoder', 'DistancePointBBoxCoder', 'DeltaXYWHBBoxCoderForGLIP', 'YOLOXBBoxCoder'
 ]
